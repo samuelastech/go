81c8f8162ca4666eeac6d3caf53b274d489c27de
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-02-28 15:20:21 -0300

    feat(code): deck.toString()

--- code/main.go
+++ code/main.go
@@ -1,8 +1,8 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
-	hand, remainingCards := deal(cards, 3)
-	hand.print()
-	remainingCards.print()
+	fmt.Println(cards.toString())
 }
